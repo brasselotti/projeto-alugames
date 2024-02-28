2b791777ac1187afccc6923484b03c40757c6008
Fabiano Brasselotti <brasselottif@gmail.com>
2024-02-28 17:49:15 -0300

    Adicionando pop-up de confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -5,17 +5,18 @@ function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-
-    // let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
     // alert(nomeJogo.innerHTML);
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Você tem certeza que deseja devolver o jogo "${nomeJogo.textContent}"?`)){
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
         quantidadeAlugada -= 1;
         console.log(quantidadeAlugada);
-        alert('Jogo devolvido');         
+        alert('Jogo devolvido');
+        }         
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
