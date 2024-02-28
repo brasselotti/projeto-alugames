1a8094f78325ec8b3cbd7b539e71ac7352870c75
Fabiano Brasselotti <brasselottif@gmail.com>
2024-02-28 17:38:05 -0300

    Adicionando alerta de quantidade de aluguéis no console

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+var quantidadeAlugada = 1;
+console.log(quantidadeAlugada);
+
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
@@ -10,11 +13,14 @@ function alterarStatus(id){
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
-        alert('Jogo devolvido');
-               
+        quantidadeAlugada -= 1;
+        console.log(quantidadeAlugada);
+        alert('Jogo devolvido');         
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';       
+        botao.textContent = 'Devolver';
+        quantidadeAlugada += 1;
+        console.log(quantidadeAlugada);       
     }
 }
